94346f79b480e1cc3a77ab8e9b2481b75af0b0f5
doksanbir <yalcindoksanbir@gmail.com>
2024-07-29 03:21:12 +0300

    [WIP]- Rate Limiting with Algorithms

---
+++ rate-limiter/src/test/java/com/iluwatar/ratelimiter/RateLimiterApplicationTests.java
@@ -0,0 +1,11 @@
+package com.iluwatar.ratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimiterApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
